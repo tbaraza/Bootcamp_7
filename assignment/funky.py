22725570a564d7dbe21c6022c891ae25d104023a
Tonida <tonidahnjeli@gmail.com>
2016-04-26 18:28:25 +0300

    changes on funky

--- assignment/funky.py
+++ assignment/funky.py
@@ -1,4 +1,10 @@
 def funky(a, b):
+	"""
+	takes two arguments and returns
+	the sum of the respective data types giving out necessary
+	message for data types that cannot be summed up
+	"""
+
     if type(a) == dict and type(b) == dict:
         a.update(b)
         return a
@@ -9,10 +15,12 @@ def funky(a, b):
     else:
         return a + b
 
-
+print([1, 2], {1: "me", 2: "you"})
 print funky(1.777, 34)
 print funky(34, 56)
-print funky({1: "boy"}, {2: "girl"})
+print funky({1: "boy", 3: "you"}, {3: "girl"})
 print funky("lion", 34)
 print funky(2.3444, 45.7)
 print funky("toni", "da")
+print funky([1, 2], [5, 6])
+print funky("toni", [3, 4, 5])
