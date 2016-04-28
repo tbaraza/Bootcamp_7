29b8db9be22607d4f097c71a2088a286775c69fd
Tonida <tonidahnjeli@gmail.com>
2016-04-28 17:11:12 +0300

    packages and TDD

--- Andelabs/word_count.py
+++ Andelabs/word_count.py
@@ -1,13 +1,16 @@
 def word_count(a):
-    if isinstance(a, str):
-        a = a.split()
-
-
-return "{} : {}".format(i, a.count(i))
-
-print word_count("word count")
+	if isinstance(a, str):
+		a = a.split()
+		for i in a:
+			n = ''
+			n += "{} : {}".format(i, a.count(i))
+	return n
+print word_count("word count word book")
 
 # a = "mine two"
 # print (a).split()
 # b = a.split()
 # print b.count(i) for i in b
+# a = a.split()
+#     for i in a:
+#     	if isinstance(i, a):return "{} : {}".format(i, a.count(i))
