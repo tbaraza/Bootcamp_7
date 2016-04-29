29b410a8f57bec55f58f44e3939ba8e599b61c16
Tonida <tonidahnjeli@gmail.com>
2016-04-29 17:20:34 +0300

    word coount

--- Andelabs/word_count.py
+++ Andelabs/word_count.py
@@ -1,16 +1,54 @@
-def word_count(a):
-	if isinstance(a, str):
-		a = a.split()
-		for i in a:
-			n = ''
-			n += "{} : {}".format(i, a.count(i))
-	return n
-print word_count("word count word book")
+def words(string):
+    """
+    The function counts the word occurence in a string
+        e.g. For "olly olly in come free"
 
-# a = "mine two"
-# print (a).split()
-# b = a.split()
-# print b.count(i) for i in b
-# a = a.split()
-#     for i in a:
-#     	if isinstance(i, a):return "{} : {}".format(i, a.count(i))
+            olly: 2
+            in: 1
+            come: 1
+            free: 1
+    """
+    new_list = string.split()
+    string_dict = {}
+
+    for word in new_list:
+        if word.isdigit(): 
+            word = int(word)
+
+        if word in string_dict:
+            string_dict[word] = string_dict[word] + 1
+
+        else:
+            string_dict[word] = 1
+      return string_dict
+
+print words("Hi hi hi higher car carpet how are : : yah")
+ 
+
+Click here to type a chat message. Supports GitHub flavoured markdown.
+
+
+
+def find_max_min(A):
+    max_, min_ = A[0], A[0]
+    a = []
+    for i in A:
+        if i > max_:
+            max_ = i
+
+        if i < min_:
+            min_ = i
+
+    if max_ != min_:
+    	a.append(max_)
+    	a.append(min_)
+
+    if max_ == min_:
+    	a.append(len(A))
+
+    return a
+  
+
+print find_max_min([2, 4, 6, 8, 78, 90])
+print find_max_min([4, 6])
+print find_max_min([1, 1, 1])
